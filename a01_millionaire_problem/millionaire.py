58f1e68afa4bbcc53d4574ef661fb5ab77880658
Peng Gu <penggu@gmail.com>
2021-07-08 07:36:33 -0700

    add stub file for millionaire problem

---
+++ a01_millionaire_problem/millionaire.py
@@ -0,0 +1,13 @@
+banner = '''This project is inspired by a youtube video:
+姚期智百万富翁问题：大数据时代，如何保护个人隐私？
+URL: https://youtu.be/dOTwAzXrkyQ
+'''
+
+def print_banner():
+    print(banner)
+
+def main():
+    print_banner()
+
+if __name__ == "__main__":
+    main()
